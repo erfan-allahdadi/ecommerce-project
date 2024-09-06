134dc2f755bb1302ea97ac57a446769d597f22db
erfanallahdadi <erfanallahdadi@gmail.com>
2024-09-06 13:01:48 +0330

    add navbar and dashboard page and layout

--- hooks/useStoreModal.ts
+++ hooks/useStoreModal.ts
@@ -7,7 +7,7 @@ interface UseStoreModalStore {
 }
 
 export const useStoreModal = create<UseStoreModalStore>((set) => ({
-  isOpen: true,
+  isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
